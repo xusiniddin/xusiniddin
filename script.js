36cf4a4403db0db794c40512c5dd7eeae2c68974
xusiniddin <test>
2022-11-30 09:51:32 +0300

    script

---
+++ script.js
@@ -0,0 +1,29 @@
+var ism= prompt("Ismingizni kriting" )
+console.log("Sizning ismingiz "+ism);
+var yosh= prompt("Yoshingizni  kriting" )
+console.log("Sizning yoshingiz "+yosh);
+var qoshish= +prompt("20+20")
+if (qoshish == 40) {
+    console.log("Sizning javob togri "+qoshish);
+}else{
+    console.log("Sizning javob notogri "+qoshish+" Togri javob 40"); 
+}
+var ayirish= +prompt("20-10")
+if (ayirish == 10) {
+    console.log("Sizning javob togri "+ayirish);
+}else{
+    console.log("Sizning javob notogri "+ayirish+" Togri javob 10"); 
+}
+var qoshish2= +prompt("8*6")
+if (qoshish2 == 48) {
+    console.log("Sizning javob togri "+qoshish2);
+}else{
+    console.log("Sizning javob notogri "+qoshish2+" Togri javob 48"); 
+}
+var ayirish2= +prompt("50/2")
+if (ayirish2 == 25) {
+    console.log("Sizning javob togri "+ayirish2);
+}else{
+    console.log("Sizning javob notogri "+ayirish2+" Togri javob 25"); 
+}
+alert("Hamma bergan javoblaringizni consolda koring")
